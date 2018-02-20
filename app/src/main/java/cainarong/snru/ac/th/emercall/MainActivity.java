c8ab34e92ac86bc825d0d4ff50683133ce587156
AzSuKer <AzSuKer@gmail.com>
2018-02-20 12:01:32 +0700

    Add Fragment Success

--- app/src/main/java/cainarong/snru/ac/th/emercall/MainActivity.java
+++ app/src/main/java/cainarong/snru/ac/th/emercall/MainActivity.java
@@ -3,11 +3,23 @@ package cainarong.snru.ac.th.emercall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cainarong.snru.ac.th.emercall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contenMainFragent, new MainFragment()).commit();
+
+        }
+
+
+    }   // Main Method
+
+}   // Main Class
